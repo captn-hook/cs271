95a5416a9ca4cae42c5d2a60148d807773c6a191
Tristan Hook <tristanskyhook@gmail.com>
2022-09-22 16:27:52 -0700

    first cploration

---
+++ cploration/c00/main.c
@@ -0,0 +1,13 @@
+/******************************************
+* C-ploration 0 for CS 271
+*
+* Tristan Hook
+* FALL 9/22/2022
+******************************************/
+#include <stdio.h>
+int main() {
+    //prints 
+    printf("Hello, World!\n");
+    // exits with no error
+    return 0;
+    }
